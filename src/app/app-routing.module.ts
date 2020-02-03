a89533d8babc5edece78d819269058f2921e4c25
majesiu <pawel.majewski@mail.com>
2020-02-03 20:52:08 +0100

    basic routing to create player module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreatePlayerComponent } from './create-player/create-player.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'create', component: CreatePlayerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
