23d178dc0fa46cdd0997fa5d47a0418e841917ba
majesiu <pawel.majewski@mail.com>
2020-02-17 21:04:50 +0100

    Basic table display

--- src/app/update-player/update-player.component.ts
+++ src/app/update-player/update-player.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface UpdateTask {
+  name: string;
+  position: number;
+  tpe: number;
+  link: string;
+}
+
+const ELEMENT_DATA: UpdateTask[] = [
+  {position: 1, name: '', tpe: 3, link: ''},
+];
+
 @Component({
   selector: 'app-update-player',
   templateUrl: './update-player.component.html',
   styleUrls: ['./update-player.component.scss']
 })
 export class UpdatePlayerComponent implements OnInit {
+
+  displayedColumns: string[] = ['position', 'name', 'link', 'tpe'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
