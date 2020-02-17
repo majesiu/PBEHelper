1aa83ea38e71963bd96bd0c1a1604c306dda54b8
majesiu <pawel.majewski@mail.com>
2020-02-17 20:51:50 +0100

    Routing and button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreatePlayerComponent } from './create-player/create-player.component';
+import { UpdatePlayerComponent } from './update-player/update-player.component';
 
 
 const routes: Routes = [ 
   { path: '', redirectTo: '/create', pathMatch: 'full' },
-  { path: 'create', component: CreatePlayerComponent }];
+  { path: 'create', component: CreatePlayerComponent },
+  { path: 'update', component: UpdatePlayerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
