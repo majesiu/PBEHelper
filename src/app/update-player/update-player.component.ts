10cf958ffe0583f527031afd483fed920cea7ad2
majesiu <pawel.majewski@mail.com>
2020-02-20 22:02:04 +0100

    removed TPE underline

--- src/app/update-player/update-player.component.ts
+++ src/app/update-player/update-player.component.ts
@@ -15,8 +15,7 @@ const ELEMENT_DATA: UpdateTask[] = [
 @Component({
   selector: 'app-update-player',
   templateUrl: './update-player.component.html',
-  styleUrls: ['./update-player.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./update-player.component.scss']
 })
 export class UpdatePlayerComponent implements OnInit {
 
