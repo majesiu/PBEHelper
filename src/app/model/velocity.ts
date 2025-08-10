213cca9a323def19256e05a71c4dab0c4a6eff74
majesiu <pawel.majewski@mail.com>
2025-08-10 10:32:56 +0200

    Velocity scale cost change

--- src/app/model/velocity.ts
+++ src/app/model/velocity.ts
@@ -27,61 +27,61 @@ export class Velocity {
         return 0;
         break;
       case '80 - 83':
-        return 30;
+        return 15;
         break;
       case '83 - 85':
-        return 60;
+        return 30;
         break;
       case '84 - 86':
-        return 90;
+        return 45;
         break;
       case '85 - 87':
-        return 120;
+        return 60;
         break;
       case '86 - 88':
-        return 150;
+        return 75;
         break;
       case '87 - 89':
-        return 180;
+        return 90;
         break;
       case '88 - 90':
-        return 210;
+        return 105;
         break;
       case '89 - 91':
-        return 240;
+        return 120;
         break;
       case '90 - 92':
-        return 270;
+        return 135;
         break;
       case '91 - 93':
-        return 300;
+        return 150;
         break;
       case '92 - 94':
-        return 330;
+        return 165;
         break;
       case '93 - 95':
-        return 360;
+        return 180;
         break;
       case '94 - 96':
-        return 390;
+        return 195;
         break;
       case '95 - 97':
-        return 420;
+        return 210;
         break;
       case '96 - 98':
-        return 450;
+        return 225;
         break;
       case '97 - 99':
-        return 480;
+        return 240;
         break;
       case '98 - 100':
-        return 510;
+        return 255;
         break;
       case '99 - 101':
-        return 540;
+        return 270;
         break;
       case '100':
-        return 570;
+        return 285;
         break;
     }
   }
